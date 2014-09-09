e1ad61876e2d97577c38e72c4728f5eb7f28860b
cp612sh <cp612sh@gmail.com>
2014-09-09 16:32:19 +0800

    添加校验判断

--- WebApplication1/Controllers/CartController.cs
+++ WebApplication1/Controllers/CartController.cs
@@ -34,47 +34,57 @@ namespace Koo.Web.Controllers
         [ValidateAntiForgeryToken]
         public JsonResult Remove(string cartItemViewId, string Id, string amount)
         {
-            List<CartItem> cartItems = new List<CartItem>();
-            int count = 0;
-            float totalAmount = 0;
-           
-            try
+            if (ModelState.IsValid)
             {
-                if (HttpContext.Session["CurrentCart"] != null)
+                List<CartItem> cartItems = new List<CartItem>();
+                int count = 0;
+                float totalAmount = 0;
+
+                try
                 {
-                    cartItems = (List<CartItem>)HttpContext.Session["CurrentCart"];
-                }
-                CartItem tobeRemoved = cartItems.First(c => (c.ProjectId == int.Parse(Id) && c.Amount == amount));
-                if (tobeRemoved != null)
-                {
-                    cartItems.Remove(tobeRemoved);
-                    HttpContext.Session["CurrentCart"] = cartItems;
-                    count = cartItems.Count;
-                    foreach (var item in cartItems)
+                    if (HttpContext.Session["CurrentCart"] != null)
                     {
-                        totalAmount += float.Parse(item.Amount);
+                        cartItems = (List<CartItem>)HttpContext.Session["CurrentCart"];
+                    }
+                    CartItem tobeRemoved = cartItems.First(c => (c.ProjectId == int.Parse(Id) && c.Amount == amount));
+                    if (tobeRemoved != null)
+                    {
+                        cartItems.Remove(tobeRemoved);
+                        HttpContext.Session["CurrentCart"] = cartItems;
+                        count = cartItems.Count;
+                        foreach (var item in cartItems)
+                        {
+                            totalAmount += float.Parse(item.Amount);
+                        }
                     }
                 }
-            }
-            catch (Exception)
-            {
+                catch (Exception)
+                {
+                    return Json(new
+                    {
+                        Success = false,
+                        //Message = "ok",
+                        //Count = count,
+                        Amount = amount
+                    });
+                }
+
                 return Json(new
                 {
-                    Success = false,
-                    //Message = "ok",
-                    //Count = count,
-                    Amount = amount
+                    Success = true,
+                    Message = "ok",
+                    Count = count,
+                    CartItemId = cartItemViewId,
+                    Amount = amount,
+                    TotalAmount = totalAmount
                 });
             }
-
             return Json(new
             {
-                Success = true,
-                Message = "ok",
-                Count = count,
-                CartItemId = cartItemViewId,
-                Amount = amount,
-                TotalAmount = totalAmount
+                Success = false,
+                //Message = "ok",
+                //Count = count,
+                Amount = amount
             });
         }
 
@@ -82,38 +92,43 @@ namespace Koo.Web.Controllers
         [ValidateAntiForgeryToken]
         public JsonResult Add(string Id, string amount)
         {
-
-            bool hasError = false;
-            List<CartItem> cartItems = new List<CartItem>();
-            int count = 0;
-            try
+            if (ModelState.IsValid)
             {
-                if (HttpContext.Session["CurrentCart"] != null)
+
+                bool hasError = false;
+                List<CartItem> cartItems = new List<CartItem>();
+                int count = 0;
+                try
                 {
-                    cartItems = (List<CartItem>)HttpContext.Session["CurrentCart"];
+                    if (HttpContext.Session["CurrentCart"] != null)
+                    {
+                        cartItems = (List<CartItem>)HttpContext.Session["CurrentCart"];
+                    }
+
+                    cartItems.Add(new CartItem { ProjectId = int.Parse(Id), Amount = amount });
+                    count = cartItems.Count;
+                    HttpContext.Session["CurrentCart"] = cartItems;
+                }
+                catch
+                {
+                    hasError = true;
                 }
 
-                cartItems.Add(new CartItem { ProjectId = int.Parse(Id), Amount = amount });
-                count = cartItems.Count;
-                HttpContext.Session["CurrentCart"] = cartItems;
-            }
-            catch
-            {
-                hasError = true;
-            }
+                if (hasError)
+                {
+                    return Json(new { Success = false });
+                }
 
-            if (hasError)
-            {
-                return Json(new { Success = false });
+                return Json(new
+                {
+                    Success = true,
+                    Message = "ok",
+                    Count = count,
+                    Amount = amount
+                });
             }
-
-            return Json(new
-            {
-                Success = true,
-                Message = "ok",
-                Count = count,
-                Amount = amount
-            });
+            return Json(new { Success = false });
+            
         }
 
         protected override void Dispose(bool disposing)
