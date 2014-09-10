8fb222e090b081f72571a2f0d22b4a25f3c49797
cp612sh <cp612sh@gmail.com>
2014-09-10 17:21:12 +0800

    用户基础信息

--- WebApplication1/Models/IdentityModels.cs
+++ WebApplication1/Models/IdentityModels.cs
@@ -5,6 +5,31 @@ namespace Koo.Web.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        /// <summary>
+        /// 全名
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// 姓氏
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// 13位中国公民身份证号码
+        /// </summary>
+        public string IDNumber { get; set; }
+
+        /// <summary>
+        /// 手机号码
+        /// </summary>
+        public string MobileNumber { get; set; }
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
